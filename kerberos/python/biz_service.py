023be3ea9cba03a76e0b2292ffac3da9f0f09da2
fangliang <f304646673@gmail.com>
2024-11-04 22:52:36 +0800

    Refactor biz_service.py to improve variable naming for timestamps and enhance authentication checks for service ticket validity

--- kerberos/python/biz_service.py
+++ kerberos/python/biz_service.py
@@ -1,4 +1,5 @@
-# server.py
+# biz_service.py
+import time
 import socket
 from config import Config
 from utils import decrypt
@@ -16,21 +17,28 @@ def handle_client(client_socket):
     storage = BizServiceStorage()
     # Decrypt Service Ticket
     encrypted_biz_service_ticket = base64.b64decode(encrypted_biz_service_ticket_base64)
-    print(f'Encrypted Biz Service Ticket: {encrypted_biz_service_ticket}')
     client_name_from_biz_service_ticket, client_ip_from_biz_service_ticket, server_ip_from_biz_service_ticket, \
-        timestamp_from_biz_service_ticket, st_timestamp_from_biz_service_ticket, client_to_biz_service_session_key \
+        ticket_granting_service_to_client_timestamp_from_biz_service_ticket, biz_service_ticket_validity_from_biz_service_ticket, client_to_biz_service_session_key \
             = BizServiceTicket(storage.get_private_key()).parse_service_ticket(encrypted_biz_service_ticket)
 
     # Decrypt Authenticator
     print(f'Client to Biz Service Session Key: {client_to_biz_service_session_key}')
-    client_name_from_session , client_ip_from_session, timestamp_from_session, st_timestamp_from_authenticator = \
+    client_name_from_session , client_ip_from_session, ticket_granting_service_to_client_timestamp_from_session, biz_service_ticket_validity_from_authenticator = \
         ClientToBizServiceSession(client_to_biz_service_session_key).parse_session(encrypted_client_to_biz_service_session)
 
     if client_name_from_biz_service_ticket != client_name_from_session \
             or client_ip_from_biz_service_ticket != client_ip_from_session \
-            or timestamp_from_biz_service_ticket != timestamp_from_session:
-        print(f"Client ID: {client_name_from_biz_service_ticket}, Client IP: {client_ip_from_biz_service_ticket}, Timestamp: {timestamp_from_biz_service_ticket}")
-        print(f"Client ID: {client_name_from_session}, Client IP: {client_ip_from_session}, Timestamp: {timestamp_from_session}")
+            or ticket_granting_service_to_client_timestamp_from_biz_service_ticket != ticket_granting_service_to_client_timestamp_from_session\
+            or biz_service_ticket_validity_from_biz_service_ticket != biz_service_ticket_validity_from_authenticator:
+        print(f"Client ID: {client_name_from_biz_service_ticket}, Client IP: {client_ip_from_biz_service_ticket}, Timestamp: {ticket_granting_service_to_client_timestamp_from_biz_service_ticket}")
+        print(f"Client ID: {client_name_from_session}, Client IP: {client_ip_from_session}, Timestamp: {ticket_granting_service_to_client_timestamp_from_session}")
+        response = "Authentication Failed"
+        client_socket.send(response.encode('utf-8'))
+        client_socket.close()
+        return
+    
+    if int(biz_service_ticket_validity_from_biz_service_ticket) < int(time.time()):
+        print("Service Ticket expired.")
         response = "Authentication Failed"
         client_socket.send(response.encode('utf-8'))
         client_socket.close()
